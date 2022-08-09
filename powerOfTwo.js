a078076edcae8eab46d1f50fa8ece87f86183a83
Brad Fujizawa <bafujizawa@gmail.com>
2022-08-09 10:33:49 -0700

    Added code to make it functional

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -1,14 +1,29 @@
-// Return true if n is a power of 2
+// Given an integer n, return true if it is a power of two. Otherwise, return false
+// An integer n is a power of two, if there exists an integer x such that n == 2x
 
-// n = 3, false
-// n = 4, true
-// n = -1, false
+n = 1
+// Output: true
 
+n = 16
+// Output: true
+
+n = 3
+// Output: false
 
 var isPowerOfTwo = function(n) {
-    
+    if(n == 1 || n == 0) return true;
+    while(n != 1 ) {
+        n = n / 2;
+        if(n % 2 != 0 && n != 1) {
+            return false;
+        }
+    }
+    return true;
 };
 
-for(i=-25;i<25;i++) {
-    console.log(2 ** i)
-}
+console.log(isPowerOfTwo(0));
+console.log(isPowerOfTwo(1));
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(3));
+console.log(isPowerOfTwo(16));
+
